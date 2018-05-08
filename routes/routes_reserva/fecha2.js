7bfa75302d101568c7ef54a97a1dc44406e04b89
sebastianMof <sebastian.mofre@gmail.com>
2018-05-08 15:39:03 -0300

    momentjsTesting

--- routes/routes_reserva/fecha2.js
+++ routes/routes_reserva/fecha2.js
@@ -5,6 +5,8 @@ const models = require('../../models');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
+
+
 router.post('/',async(req, res, next)=>{
   let dia = req.query.dia;
   let mes = req.query.mes;
@@ -12,11 +14,20 @@ router.post('/',async(req, res, next)=>{
   let hora = req.query.hora;
   let min = req.query.min;
   let fecha1 = new Date(parseInt(anyo), parseInt(mes), parseInt(dia), parseInt(hora), parseInt(min));
-  let fecha2 = new Date(parseInt(anyo), parseInt(mes), parseInt(dia), parseInt(hora)+3, parseInt(min));
+  let fecha2 = new Date(parseInt(anyo), parseInt(mes), parseInt(dia), parseInt(hora)+1, parseInt(min));
+  let fecha3 = new Date(parseInt(anyo), parseInt(mes), parseInt(dia), parseInt(hora)+3, parseInt(min));
+  var fechaMoment1 = moment(fecha1);
+  var fechaMoment2 = moment(fecha2);
+  var fechaMoment3 = moment(fecha3);
+  
+  //console.log(moment(fechaMoment).format('YYYY MM DD'));
+  console.log(moment(fechaMoment2).isBetween(fechaMoment1,fechaMoment3));
+  moment(fechaMoment1).toDate().
 
   let json = {
   	fecha1:fecha1,
-  	fecha2:fecha2
+  	fecha2:fechaMoment1
+
   }
   res.send(json);
 });
