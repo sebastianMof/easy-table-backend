06c55d045d627f9d3170743949faaf2b53b8c9c9
Sebastian <sebastian.mofre@gmail.com>
2018-06-14 11:59:56 -0400

    Agregado cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 //--------routes---------------
 const indexRouter = require('./routes/index');
 const mesaRouter = require('./routes/mesa');
@@ -15,7 +16,7 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -43,5 +44,7 @@ app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.render('error');
 });
+
+
 
 module.exports = app;
